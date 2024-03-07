a88e4bbea30e3eb6925d915baa4cbc509c9e2a24
mcakyerima <mcakyerima@gmail.com>
2024-03-07 23:28:29 +0100

    fixed classroom class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,18 +1,15 @@
 /**
  * Class Representing a classroom.
- * @class
  */
 export default class ClassRoom {
     /**
      * Creates a new ClassRoom instance.
-     * @constructor
      * @param {number} maxStudentsSize - The maximum number of students in the class.
      */
     constructor(maxStudentsSize) {
       /**
        * The maximum number of students allowed in the class.
        * @type {number}
-       * @private
        */
       this._maxStudentsSize = maxStudentsSize;
     }
