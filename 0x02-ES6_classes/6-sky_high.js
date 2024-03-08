835011908fcdad1716057c13dc4b6fee2ea1da94
mcakyerima <mcakyerima@gmail.com>
2024-03-08 03:02:05 +0100

    finished building class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,36 @@
+import Building from './5-building.js';
+
+/**
+ * Represents a sky-high building.
+ * @author Mohammed Kaka
+ * @class SkyHighBuilding
+ * @extends Building
+ */
+export default class SkyHighBuilding extends Building {
+  /**
+   * Creates a new SkyHighBuilding instance.
+   * @constructor
+   * @param {Number} sqft - The square footage of the building.
+   * @param {Number} floors - The number of floors in the building.
+   */
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  /**
+   * Gets the number of floors in the building.
+   * @returns {Number} The number of floors.
+   */
+  get floors() {
+    return this._floors;
+  }
+
+  /**
+   * Overrides the evacuationWarningMessage method from the parent class.
+   * @returns {String} The evacuation warning message.
+   */
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
