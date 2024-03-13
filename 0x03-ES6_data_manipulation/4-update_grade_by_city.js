b5d1a8c649525c0dc810ce58d9088a77c2c22b6c
mcakyerima <mcakyerima@gmail.com>
2024-03-13 02:51:15 +0100

    ✅ finished task 5

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+/**
+ * Updates the grades of students for a specific city.
+ * @param {Array} students - An array of students object.
+ * @param {string} city - The city to filter the students by.
+ * @param {Array} newGrades - An array of objects containing new grades for students.
+ * @returns {Array} - An array of student objects with updated grades.
+ * @author Mohammed Kaka.
+ */
+export default function updateStudentGradeByCity (students, city, newGrades) {
+    return students.map(student => {
+        // find the  new grade for a specific student.
+        const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+        // update the grade for the student
+        const grade = gradeObj ? gradeObj.grade : 'N/A';
+        return {...student, grade};
+    }).filter(student => student.location === city);
+}
