d584ee21b6ed365ca209974ff47ba32a617d7470
mcakyerima <mcakyerima@gmail.com>
2024-05-01 04:09:18 +0100

    📑✅ Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,28 @@
+
+const fs = require('fs');
+
+function countStudents(path) {
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(Error('Cannot load the database'));
+      if (data) {
+        let newData = data.toString().split('\n');
+        newData = newData.slice(1, newData.length - 1);
+        console.log(`Number of students: ${newData.length}`);
+        const obj = {};
+        newData.forEach((el) => {
+          const student = el.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
+        }
+      }
+      res();
+    });
+  };
+  return new Promise(promise);
+}
+
+module.exports = countStudents;
